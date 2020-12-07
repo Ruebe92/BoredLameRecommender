ddb685039a1878467bac5ae0948f173208ddf626
Ruebe92 <38210724+Ruebe92@users.noreply.github.com>
2020-12-07 15:24:24 +0100

    Combined all parts

--- flask_app/ml_models.py
+++ flask_app/ml_models.py
@@ -3,7 +3,10 @@
 # TO-DO: Get movies from file or from database
 
 from faker import Faker
-
+import json
+import pandas as pd
+import numpy as np
+import pickle
 
 def simple_recommender(num):
     fake = Faker()
@@ -12,6 +15,20 @@ def simple_recommender(num):
 
 
 def nmf_recommender(user_input, no_of_recommendations):
+
+    to_be_deleted_keys = []
+
+    for key in user_input:
+
+        if isinstance(user_input[key], str):
+
+            to_be_deleted_keys.append(key)
+
+    for key in to_be_deleted_keys:
+
+        del user_input[key]
+
+    nmf_model = pickle.load(open("nmf_model.sav", "rb"))
     Id_input = json.load(open("game_ids.json"))
     user_ratings = pd.DataFrame(user_input, index=['DAU'], columns=Id_input.values())
     user_ratings = user_ratings.fillna(0)
@@ -19,11 +36,16 @@ def nmf_recommender(user_input, no_of_recommendations):
     user_R = pd.DataFrame(np.dot(user_P, nmf_model.components_), index=['DAU'], columns=Id_input.values())
     recommendations = user_R.drop(columns=user_input.keys())
     recommendations = recommendations.T.sort_values(by='DAU', ascending=False)
-    return recommendations.iloc[:no_of_recommendations]
+
+    output_list = list(recommendations.index[:no_of_recommendations])
+
+    return output_list
 
 def cosim_recommender():
     pass
 
 
 if __name__ == "__main__":
-    print(simple_recommender(5))
+
+    Id_input = json.load(open("user_input.json"))
+    test_recommendation = nmf_recommender(Id_input,5)
