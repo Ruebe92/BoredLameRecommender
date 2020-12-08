0ed0b9cacb6ddc9f6f3988e116e1c27df74188d5
ivoalbrecht <ivo_albrecht@hotmail.com>
2020-12-08 16:57:54 +0100

    new lay-out working for the index page
    :+1:

--- flask_app/application.py
+++ flask_app/application.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, request
 import ml_models
 import pandas as pd
-from cleanup import structure_ratings
+# from cleanup import structure_ratings
 from data_import import get_games
 
 app = Flask(__name__)
 # instantiating a Flask application
 # "__name__" is a reference to the current script (application.py)
 
-NUMBER_OF_GAMES = 10
+NUMBER_OF_GAMES = 12
+NUMBER_OF_RECOM = 4
 
 @app.route("/index")
 @app.route("/")
 def index():
+    
     game_list, image_list = get_games('rank',NUMBER_OF_GAMES)
-
 
     return render_template("index.html", games_html= zip(game_list, image_list))
 
@@ -28,11 +29,7 @@ def recommend():
 
     user_input = dict(request.args)
 
-    df = structure_ratings(user_input, 'new_user')
-    print(user_input)
-    print(df)
-
-    results = ml_models.nmf_recommender(user_input, 5)
+    results = ml_models.nmf_recommender(user_input, NUMBER_OF_RECOM)
 
     return render_template("recommendations.html", results=results)
 
