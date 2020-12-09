3e4dd074f8de680ca29f312095e4a1db8b3d9a39
Ruebe92 <38210724+Ruebe92@users.noreply.github.com>
2020-12-09 14:52:57 +0100

    Added path variable to ml_model

--- flask_app/ml_models.py
+++ flask_app/ml_models.py
@@ -6,7 +6,7 @@ import numpy as np
 import pickle
 
 
-def nmf_recommender(user_input, no_of_recommendations, model_path = "nmf_model.sav", game_ids_path = "game_ids.json"):
+def nmf_recommender(user_input, no_of_recommendations, path = ""):
     games_to_exclude = user_input.keys()
     to_be_deleted_keys = []
     for key in user_input:
@@ -14,8 +14,8 @@ def nmf_recommender(user_input, no_of_recommendations, model_path = "nmf_model.s
             to_be_deleted_keys.append(key)
     for key in to_be_deleted_keys:
         del user_input[key]
-    nmf_model = pickle.load(open(model_path, "rb"))
-    Id_input = json.load(open(game_ids_path))
+    nmf_model = pickle.load(open(path + "nmf_model.sav", "rb"))
+    Id_input = json.load(open(path + "game_ids.json"))
     user_ratings = pd.DataFrame(user_input, index=['DAU'], columns=Id_input.values())
     user_ratings = user_ratings.fillna(0)
     user_P = nmf_model.transform(user_ratings)
