95b948428c632ddaa17bfe7fb12f26ab78192b61
Ruebe92 <38210724+Ruebe92@users.noreply.github.com>
2020-12-09 12:32:07 +0100

    Revert "Updated test"
    This reverts commit 985975e7823a3160ebd11d21c1c2795da69dcee3.

--- flask_app/test_ml_models.py
+++ flask_app/test_ml_models.py
@@ -3,7 +3,7 @@ import ml_models
 
 def test_nmf_recommender_output_length():
 
-    user_input = json.load(open('flask_app/user2_input.json'))
+    user_input = json.load(open('user2_input.json'))
 
     test_recommendation = ml_models.nmf_recommender(user_input,5)
 
