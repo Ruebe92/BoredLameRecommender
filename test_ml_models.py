0e3988f1e38f8e8d6922434a16971b577a73d5ce
Ruebe92 <38210724+Ruebe92@users.noreply.github.com>
2020-12-09 14:38:10 +0100

    Added relative paths for testing

--- test_ml_models.py
+++ test_ml_models.py
@@ -1,7 +1,7 @@
 import json
 import pytest
 
-import ml_models
+from flask_app import ml_models
 
 
 @pytest.mark.parametrize('user_input', ['user1_input.json','user2_input.json','user3_input.json'])
@@ -9,7 +9,7 @@ def test_nmf_recommender_output_length(user_input):
 
     user_input = json.load(open(user_input))
 
-    test_recommendation = ml_models.nmf_recommender(user_input,5)
+    test_recommendation = ml_models.nmf_recommender(user_input,5, model_path = "flask_app/nmf_model.sav", game_ids_path = "flask_app/game_ids.json")
 
     assert  len(test_recommendation) == 5
 
