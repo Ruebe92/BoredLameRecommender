3a5955448b731b5d40a488186d8683666e9cff75
Ruebe92 <38210724+Ruebe92@users.noreply.github.com>
2020-12-09 14:21:59 +0100

    Updated requirements.txt

--- flask_app/test_ml_models.py
+++ flask_app/test_ml_models.py
@@ -1,12 +1,18 @@
 import json
+import pytest
+
 import ml_models
 
-def test_nmf_recommender_output_length():
 
-    user_input = json.load(open('user2_input.json'))
+@pytest.mark.parametrize('user_input', ['user1_input.json','user2_input.json','user3_input.json'])
+def test_nmf_recommender_output_length(user_input):
+
+    user_input = json.load(open(user_input))
 
     test_recommendation = ml_models.nmf_recommender(user_input,5)
 
     assert  len(test_recommendation) == 5
+
+
 
 
